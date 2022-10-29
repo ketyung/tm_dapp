1fe28488fde2d0dc96a61e9eb511b566416fc390
techchee <ketyung@techchee.com>
2022-10-29 10:10:15 +0800

    update

---
+++ src/views/collection/PriceTypeRow.tsx
@@ -0,0 +1,49 @@
+import { FC } from "react";
+import { TicketType } from "../../models";
+import { FormInput } from "../components/FormInput";
+import { CollectionFormProps } from "./Form";
+
+
+type Props = CollectionFormProps & {index ? : number, ticketType? : TicketType };
+
+export const PriceTypeRow : FC <Props> = ({
+    index, collection, setCollection, ticketType
+
+}) =>{
+
+    return <tr key={"ptype"+index}>
+    <td valign="top" style={{width:"2%"}}>{(index ?? 0)+1}</td>
+    <td valign="top" style={{width:"40%"}}>
+        <FormInput style={{maxWidth:"150px"}} 
+        formItemStyle={{display:"inline"}}
+        value={ticketType?.ticket_type} onChange={(e)=>{
+            if ( setCollection && collection && collection.ticket_types && index) {
+                let tts = collection.ticket_types;
+                tts[index].ticket_type = e.target.value;
+                setCollection({...collection, ticket_types: tts });
+            }
+        }}/>
+
+    </td>
+    <td valign="top" style={{width:"35%",textAlign:"left",
+    paddingLeft:"12px"}} colSpan={2}>
+        <FormInput style={{maxWidth:"80px"}} 
+        formItemStyle={{display:"inline"}}
+        isNumber={true} value={ticketType?.price} onChange={(e)=>{
+            if ( setCollection && collection && collection.ticket_types && index) {
+                let tts = collection.ticket_types;
+                let price = e;
+
+                if (!isNaN(price)) {
+               
+                    tts[index].price = price;
+                    setCollection({...collection, ticket_types: tts });
+                   
+                }
+              
+            }
+        }}/>
+    </td>
+    </tr>;
+
+}
--- src/views/collection/PriceTypesForm.tsx
+++ src/views/collection/PriceTypesForm.tsx
@@ -1,6 +1,6 @@
 import { FC, useEffect } from "react";
 import { CollectionFormProps } from "./Form";
-import { FormInput } from "../components/FormInput";
+import { PriceTypeRow } from "./PriceTypeRow";
 import { PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
 import './css/PriceTypesForm.css';
 
@@ -60,57 +60,20 @@ export const PriceTypesForm : FC <CollectionFormProps> = ({
             <tr>             
                 <th style={{width:"2%"}}>No.</th>
                 <th style={{width:"40%",textAlign:"left",paddingLeft:"20px"}}>Price Type</th>
-                <th style={{width:"25%"}}>Price In NEAR</th>
-                <th style={{width:"20%"}}><PlusCircleOutlined
+                <th style={{width:"20%"}}>Price In NEAR</th>
+                <th style={{width:"20%"}}>Color Code</th>
+                <th style={{width:"15%"}}><PlusCircleOutlined
                 onClick={()=>{ addMoreRows();}}/>
                 <MinusCircleOutlined style={{marginLeft:"20px"}}
                 onClick={()=>{ removeLastRow();}}/>
                 </th>
             </tr>
         </thead>
-
         <tbody>
-               
         {
-            collection?.ticket_types?.map((e,i) =>{
-
-                    return <tr key={"ptype"+i}>
-                        <td valign="top" style={{width:"2%"}}>{i+1}</td>
-                        <td valign="top" style={{width:"40%"}}>
-                            <FormInput style={{maxWidth:"150px"}} 
-                            formItemStyle={{display:"inline"}}
-                            value={e.ticket_type} onChange={(e)=>{
-                                if ( setCollection && collection && collection.ticket_types) {
-                                    let tts = collection.ticket_types;
-                                    tts[i].ticket_type = e.target.value;
-                                    setCollection({...collection, ticket_types: tts });
-                                }
-                            }}/>
-
-                        </td>
-                        <td valign="top" style={{width:"35%",textAlign:"left",
-                        paddingLeft:"12px"}} colSpan={2}>
-                            <FormInput style={{maxWidth:"100px"}} 
-                            formItemStyle={{display:"inline"}}
-                            isNumber={true} value={e.price} onChange={(e)=>{
-                                if ( setCollection && collection && collection.ticket_types) {
-                                    let tts = collection.ticket_types;
-                                    let price = e;
-
-                                    if (!isNaN(price)) {
-                                   
-                                        tts[i].price = price;
-                                        setCollection({...collection, ticket_types: tts });
-                                       
-                                    }
-                                  
-                                }
-                            }}/>
-
-                        </td>
-
-    
-                    </tr>
+            collection?.ticket_types?.map((t,i) =>{
+                return <PriceTypeRow collection={collection}
+                setCollection={setCollection} index={i} ticketType={t}/>
             })
         }
         </tbody>
