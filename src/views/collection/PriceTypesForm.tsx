2efe7d0ed5af97ec837e3ed8d82a251133f5c6d9
techchee <ketyung@techchee.com>
2022-10-29 08:11:16 +0800

    update PriceTypesForm

--- src/views/collection/PriceTypesForm.tsx
+++ src/views/collection/PriceTypesForm.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect } from "react";
 import { CollectionFormProps } from "./Form";
 import { FormInput } from "../components/FormInput";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import { PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
 import './css/PriceTypesForm.css';
 
 export const PriceTypesForm : FC <CollectionFormProps> = ({
@@ -28,8 +28,12 @@ export const PriceTypesForm : FC <CollectionFormProps> = ({
         if ( collection && setCollection ) {
 
             let tts = collection.ticket_types;
+
+            let idx = tts?.length ?? 0; 
+            let idxs = idx > 1 ? " " + idx  : "";
+
             tts?.push ({
-                ticket_type : "Premium",
+                ticket_type : "Premium" + idxs,
                 price : 2.00
             });
 
@@ -37,26 +41,45 @@ export const PriceTypesForm : FC <CollectionFormProps> = ({
         }
     }
 
+    const removeLastRow = () =>{
+
+        if ( collection && setCollection ) {
+
+            let tts = collection.ticket_types;
+            if ( tts && tts.length > 1) {
+                tts?.pop();
+                setCollection({...collection, ticket_types : tts});
+            }
+        }
+    }
+
 
     return <div className="PriceTypesForm">
-        <div className="header">
-            <div className="row">
-                <div className="col" style={{width:"2%"}}>No.</div>
-                <div className="col" style={{width:"40%"}}>Price Type</div>
-                <div className="col" style={{width:"25%"}}>Price In NEAR</div>
-                <div className="col" style={{width:"10%"}}><PlusCircleOutlined
-                onClick={()=>{ addMoreRows();}}/></div>
-            </div>
-        </div>
-        <div className="body">
+        <table style={{width:"99%"}} cellPadding={2} cellSpacing={2}>
+        <thead>   
+            <tr>             
+                <th style={{width:"2%"}}>No.</th>
+                <th style={{width:"40%",textAlign:"left",paddingLeft:"20px"}}>Price Type</th>
+                <th style={{width:"25%"}}>Price In NEAR</th>
+                <th style={{width:"20%"}}><PlusCircleOutlined
+                onClick={()=>{ addMoreRows();}}/>
+                <MinusCircleOutlined style={{marginLeft:"20px"}}
+                onClick={()=>{ removeLastRow();}}/>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
                
         {
             collection?.ticket_types?.map((e,i) =>{
 
-                    return <div className="row item" key={"ptype"+i}>
-                        <div className="col" style={{width:"2%"}}>{i+1}</div>
-                        <div className="col" style={{width:"40%"}}>
-                            <FormInput style={{maxWidth:"150px"}} value={e.ticket_type} onChange={(e)=>{
+                    return <tr key={"ptype"+i}>
+                        <td valign="top" style={{width:"2%"}}>{i+1}</td>
+                        <td valign="top" style={{width:"40%"}}>
+                            <FormInput style={{maxWidth:"150px"}} 
+                            formItemStyle={{display:"inline"}}
+                            value={e.ticket_type} onChange={(e)=>{
                                 if ( setCollection && collection && collection.ticket_types) {
                                     let tts = collection.ticket_types;
                                     tts[i].ticket_type = e.target.value;
@@ -64,9 +87,12 @@ export const PriceTypesForm : FC <CollectionFormProps> = ({
                                 }
                             }}/>
 
-                        </div>
-                        <div className="col" style={{width:"25%"}}>
-                            <FormInput style={{maxWidth:"100px"}} isNumber={true} value={e.price} onChange={(e)=>{
+                        </td>
+                        <td valign="top" style={{width:"35%",textAlign:"left",
+                        paddingLeft:"12px"}} colSpan={2}>
+                            <FormInput style={{maxWidth:"100px"}} 
+                            formItemStyle={{display:"inline"}}
+                            isNumber={true} value={e.price} onChange={(e)=>{
                                 if ( setCollection && collection && collection.ticket_types) {
                                     let tts = collection.ticket_types;
                                     let price = e;
@@ -81,13 +107,14 @@ export const PriceTypesForm : FC <CollectionFormProps> = ({
                                 }
                             }}/>
 
-                        </div>
+                        </td>
 
     
-                    </div>
+                    </tr>
             })
         }
-        </div>
+        </tbody>
+        </table>
        
     </div>
 }
