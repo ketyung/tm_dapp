0c7830944698fac5a886322bf33f135b87f952dd
ketyung <ketyung@techchee.com>
2022-11-01 09:51:41 +0800

    update with page Template1

--- src/views/salesPage/templates/Template1.tsx
+++ src/views/salesPage/templates/Template1.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import { Button } from "antd";
 import { Collection, ShortCorrectionInfo } from "../../../models";
+import {Helmet} from "react-helmet";
+import './css/Template1.css';
 
 type Props = {
 
@@ -17,11 +19,18 @@ export const Template1 : FC <Props> = ({
 }) =>{
 
     return <div className="Template1">
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Mint your ticket for {shortCollectionInfo?.collectionId?.title}</title>       
+        </Helmet>
+        <Helmet bodyAttributes={{style: ' background-image:linear-gradient(to right, #223 , #345)'}}/>
         <h3>Buy your ticket for</h3>
         <h2>{shortCollectionInfo?.collectionId?.title}</h2>
+        <div><img src={shortCollectionInfo?.icon} style={{width:"300px",height:"auto",
+        borderRadius:"300px",border:"10px solid #aab"}}/></div>
         { !hasSignedIn ? <Button shape="round" 
         style={{background:"#349",color:"white",marginTop:"20px"}}>Connect Your Wallet</Button>
-        : <Button shape="round" style={{background:"#396",color:"white",marginTop:"20px"}}>
+        : <Button className="BuyButton">
         Buy Ticket</Button>}
     </div>
 }
