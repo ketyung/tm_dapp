23f43d000b410537882e8d239eb3da4ea0fd3f7e
techchee <ketyung@techchee.com>
2022-11-05 10:07:37 +0800

    update

--- src/views/NotSignedInView.tsx
+++ src/views/NotSignedInView.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Button } from "antd";
 import { TopNavBar } from "./NSTopNavBar";
+import { InfoCircleFilled } from "@ant-design/icons";
 import useWalletState from "../hooks/useWalletState";
 import nearLogo from '../images/near_logo_wht.png';
 import logo from '../images/logo1.png';
@@ -11,7 +12,7 @@ export const NotSignedInView : FC = () =>{
     const {signIn} = useWalletState();
 
     return <><TopNavBar/><div className="NotSignedInDiv">
-    <img src={logo} className="Logo"/>
+    <img src={logo} className="Logo" title="Ticket Mint..."/>
     
     <Button className="LaunchAppButton" onClick={()=>{
         signIn();}}>Launch App
@@ -23,5 +24,9 @@ export const NotSignedInView : FC = () =>{
     <img src={nearLogo} style={{width:"100px",height:"auto",marginLeft:"10px",marginRight:"4px"}} />
     MainNet
     </Button>
+   
+    <Button className="LaunchAppButton" type="link" href="/" 
+    icon={<div style={{paddingTop:"10px"}}>Docs <InfoCircleFilled style={{marginLeft:"10px"}}/></div>}/>
+   
     </div></>
 }
