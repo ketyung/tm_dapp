2927e252f3ea62491f5d98ffbd70b318c3961f4c
ketyung <ketyung@techchee.com>
2022-11-07 10:48:31 +0800

    update

--- src/utils/shortener.ts
+++ src/utils/shortener.ts
@@ -2,12 +2,23 @@ const SHORTENER_URL = "http://localhost:3333/";
 
 export const shorten = async (s : string) => {
 
-    let r = await ((await fetch(`${SHORTENER_URL}sv/${s}`))).json();
-    return r.v;
+    try {
+
+        let r = await ((await fetch(`${SHORTENER_URL}sv/${s}`))).json();
+        return r.v;    
+    }
+    catch(e : any) {
+        window.alert("Error::"+e.message);
+    }
 }
 
 export const longUri = async (s : string) => {
 
-    let r = await ((await fetch(`${SHORTENER_URL}lv/${s}`))).json();
-    return r.v;
+    try {
+        let r = await ((await fetch(`${SHORTENER_URL}lv/${s}`))).json();
+        return r.v;    
+    }
+    catch(e : any){
+        window.alert("Error::"+e.message);
+    }
 }
