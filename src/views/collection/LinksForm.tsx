bb0de3222cc32ba9bc7bb6ccc6093c153d4dc959
techchee <ketyung@techchee.com>
2022-11-10 14:23:32 +0800

    update

--- src/views/collection/LinksForm.tsx
+++ src/views/collection/LinksForm.tsx
@@ -1,40 +1,61 @@
-import { FC } from "react";
+import React, { FC, useState } from "react";
 import { CollectionFormProps } from "./Form";
 import { AttributeType } from "../../models";
 import { setCollectionAttribute } from "./OtherInfoForm";
 import { FormInput } from "../components/FormInput";
-import { TwitterOutlined, FacebookOutlined, LinkOutlined } from "@ant-design/icons";
+import { TwitterOutlined, FacebookOutlined, LinkOutlined,
+PlusCircleOutlined } from "@ant-design/icons";
 import './css/LinksForm.css';
+
+
+type Link = {
+
+    icon : React.ReactElement<HTMLElement>,
+
+    attributeType : AttributeType,
+}
+
+const links : Link[] = [
+    {icon : <TwitterOutlined style={{marginRight:"6px"}} title="Twitter"/>,
+    attributeType : AttributeType.Twitter},
+    {icon : <FacebookOutlined style={{marginRight:"6px"}} title="Twitter"/>,
+    attributeType : AttributeType.Facebook},
+    {icon : <LinkOutlined style={{marginRight:"6px"}} title="Twitter"/>,
+    attributeType : AttributeType.Website},
+]
+
 
 export const LinksForm : FC <CollectionFormProps> = ({
     collection, setCollection, 
 }) =>{
+
+    const [reqLinks, setReqLinks] = useState<Link[]>([
+        links[0]]);
+
+    const addLink = () =>{
+        if (reqLinks.length < links.length) {
+            setReqLinks([...reqLinks, links[reqLinks.length]]);
+        }
+    }
+
+
     return <div className="LinksForm">
-        <div>Links</div>
-        <div className="row"><FormInput style={{maxWidth:"500px"}} 
-        label={<TwitterOutlined style={{marginRight:"6px"}}/>}
-        formItemStyle={{display:"inline"}} onChange={(e)=>{
+        <div>Links <PlusCircleOutlined style={{marginLeft:"10px",cursor:"pointer"}}
+        onClick={()=>{ addLink();}}/>
+        </div>
+        {reqLinks.map(l=>{
 
-            setCollectionAttribute(AttributeType.Twitter, e.target.value, collection,
-                setCollection);
-        }} value={collection?.attributes?.filter(a => 
-        {return a.name === AttributeType.Twitter})[0]?.value}/>
-        </div>
-        <div className="row"><FormInput label={<FacebookOutlined style={{marginRight:"6px"}}/>}
-        style={{maxWidth:"500px"}} formItemStyle={{display:"inline"}} onChange={(e)=>{
-            setCollectionAttribute(AttributeType.Facebook, e.target.value, collection,
-                setCollection);
-        }} value={collection?.attributes?.filter(a => 
-        {return a.name === AttributeType.Facebook})[0]?.value}/>
-        </div>
-        <div className="row"><FormInput label={<LinkOutlined style={{marginRight:"6px"}}/>} 
-        style={{maxWidth:"500px"}} formItemStyle={{display:"inline"}} onChange={(e)=>{
+            return <div className="row"><FormInput style={{maxWidth:"500px"}} 
+            label={l.icon}
+            formItemStyle={{display:"inline"}} onChange={(e)=>{
 
-            setCollectionAttribute(AttributeType.Website, e.target.value, collection,
-                setCollection);
-        }} value={collection?.attributes?.filter(a => 
-        {return a.name === AttributeType.Website})[0]?.value}/>
-        </div>
+                setCollectionAttribute(l.attributeType, e.target.value, collection,
+                    setCollection);
+            }} value={collection?.attributes?.filter(a => 
+            {return a.name === l.attributeType})[0]?.value}/>
+            </div>
+
+        })}
 
     </div>
 }
