0e6dae60c3291172e849df81dfff1eee73027cfd
ketyung <ketyung@techchee.com>
2022-11-14 11:14:59 +0800

    update

--- src/views/TopNavBar.tsx
+++ src/views/TopNavBar.tsx
@@ -4,7 +4,7 @@ import usePage from "../hooks/usePage";
 import { Page } from "../models";
 import { Button } from "antd";
 import useWalletState from "../hooks/useWalletState";
-import { HomeOutlined } from "@ant-design/icons";
+import { HomeOutlined,QuestionCircleOutlined} from "@ant-design/icons";
 import './css/TopNavBar.css';
 
 export const TopNavBar : FC = () => {
@@ -40,6 +40,10 @@ export const TopNavBar : FC = () => {
         Balance : {balance.toFixed(3)} NEAR
         </span>
         <span className="frSpan">
+        <Button shape="circle" title="How to..." style={{color:"white",width:"30px",height:"30px",marginRight:"10px"}} 
+        icon={<QuestionCircleOutlined/>} type="link" href={process.env.REACT_APP_HOWTO_DOCS_URL} 
+        target="_blank"/>
+        
         <UsersSettingsView/>
         </span>
     </div>
