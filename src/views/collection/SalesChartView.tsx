d77f2d204b5aee4a3729025a5340cde6c1bcbbb0
techchee <ketyung@techchee.com>
2022-11-15 15:56:57 +0800

    update

--- src/views/collection/SalesChartView.tsx
+++ src/views/collection/SalesChartView.tsx
@@ -1,56 +1,72 @@
-import { FC, useEffect, useState, useCallback} from "react";
+import { FC, useEffect, useState} from "react";
 import useTicketMintsContract from "../../hooks/useTicketMintsContract";
 import { GSpinner } from "../components/GSpinner";
 import { Line } from '@ant-design/charts';
 
 export const SalesChartView : FC = () =>{
 
-    const [config,setConfig]= useState<any>();
+    const [config,setConfig]= useState<any>(
+
+        {
+            data : [{date : "", value :0}],
+            height:200,
+            step:1,
+            xField: 'date',
+            yField: 'value',
+            title: "Ticket Sales",
+            point: {
+                size: 5,
+                shape: 'circle',
+                color:"#00f",
+            },
+        }
+    );
 
     const [loading,setLoading] = useState(false);
 
+   
     const {getSalesCountInRange} = useTicketMintsContract();
 
-    const getSalesData = useCallback(async ()=>{
+    useEffect(()=>{
 
         setLoading(true);
 
-        if ( config === undefined) {
+        if ( config === undefined || config.data.length < 6) {
 
-            setTimeout(async ()=>{
-              
             
-                let data = await getSalesCountInRange();
-        
-                setConfig({
-                    data,
-                    height:200,
-                    step:1,
-                    xField: 'date',
-                    yField: 'value',
-                    title: "Ticket Sales",
-                    point: {
-                        size: 5,
-                        shape: 'circle',
-                        color:"#00f",
-                    },
+            getSalesCountInRange()
+            .then((data:any) =>{
+
+                setConfig({...config, 
+                    data
                 });
         
+                setTimeout(()=>{
+                    setLoading(false);
+                },300);         
+
+            })
+            .catch((e : Error)=>{
+
+                window.alert(e.message);
                 setLoading(false);            
+            
+            })
+    
                
-            },1000);
-        
+            
+        }
+        else {
+
+            console.log("config.is::", config, new Date());
         }
         
-    },[getSalesCountInRange]);
-
-
-    useEffect(()=>{
-        getSalesData();
     },[]);
 
 
-    const chartView = config && <Line style={{maxHeight:"250px", maxWidth:"98%"}} {...config} />;
+    
 
-    return <>{loading ? <GSpinner style={{marginTop:"30px"}} text="Loading chart..."/> : chartView }</>;
+    
+    return <>{loading ? <GSpinner style={{marginTop:"30px"}} text="Loading chart..."/> : 
+    <Line style={{maxHeight:"250px", maxWidth:"98%"}} {...config} /> }</>;
 }
